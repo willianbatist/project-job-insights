3f2552d94ac34c87bce46344d6e19870532b3c0e
Willian <willian.alves.b15@gmail.com>
2022-09-15 18:17:33 -0300

    feat: req10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,6 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    result = count_ocurrences("src/jobs.csv", "ASPCA")
+    assert(result == 59)
